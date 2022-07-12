4037780332e196116d31240164d1e0a19b53edad
HaykiOS <71113930+HaykiOS@users.noreply.github.com>
2022-07-12 15:41:59 +0400

    Update AppDelegate.swift

--- GitDemo/AppDelegate.swift
+++ GitDemo/AppDelegate.swift
@@ -13,7 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+       print("Another print statement")
+        print("Hello world")
         return true
     }
 
